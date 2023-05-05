37ae5352de39b7460e8eb92d3a5128ea0ec9d7f8
jopbdavid <jopbdavid@gmail.com>
2023-05-05 11:34:12 +0100

    updated URL

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -5,7 +5,7 @@ import { useGlobalContext } from "./context";
 
 const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_API_KEY
-} &query=`;
+}&query=`;
 console.log(import.meta.env.VITE_API_KEY);
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
